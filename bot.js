4e05d72a70c2c2d77ed48649b24bc0be81d1ae90
Klaucss <nicolaspieper@pruefster.de>
2022-04-12 23:58:15 +0200

    hotfix

--- bot.js
+++ bot.js
@@ -30,12 +30,13 @@ client.on("messageCreate", msg => {
   if (msg.content.toLowerCase().startsWith(prefix + "play")) {
          const channel = msg.member.voice.channel;
          const song = msg.content.split(" ")[1];
-
+         msg.channel.send('Searching...')
          music.play({
              interaction: msg,
              channel: channel,
              song: song
          });
+         msg.channel.send('Playing...')
       };
 });
 // music.stop is a node module that deletes the current playing audio stream with the message interaction (short = msg)
